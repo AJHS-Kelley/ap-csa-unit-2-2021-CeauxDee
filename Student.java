b8e886d9ff4a51b5b6439a9f2840ea5910cd27d2
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-11-03 09:27:24 -0400

    [5B] v1.0
    Finished making constructors

--- Student.java
+++ Student.java
@@ -13,3 +13,22 @@ public Student(){
     this.gradePointAvg = 0.0;
     this.studentName = "";
 }
+
+public Student(int studentNumber, int gradeLevel, double gradePointAvg, String studentName){
+    this.studentNumber = studentNumber;
+    this.gradeLevel = gradeLevel;
+    this.gradePointAvg = gradePointAvg;
+    this.studentName = studentName;
+}
+
+public Student(int studentNumber, String studentName){
+    this.studentNumber = studentNumber;
+    this.studentName = studentName;
+    this.gradeLevel = 9;
+    this.gradePointAvg = 4.0;
+}
+
+public static void main(String[] args){
+    Student student0 = new Student();
+    Student student1 = new Student(12345678, 11, 0.37, "Dr. Strange");
+}
