833f72fa8dab68dc0b47d60cd9578015e43711a1
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-11-03 08:51:49 -0400

    [5B] v0.5
    Made more methods

--- Methods.java
+++ Methods.java
@@ -1,6 +1,8 @@
-//Cody Collins, AP CSA Unit 2, 8:41 am, v0.0
+//Cody Collins, AP CSA Unit 2, 8:41 am, v0.5
 
 import java.util.Scanner;
+
+public static void Methods(String[] args){
 
 public class Methods {
     public static void calcGPA() {
@@ -25,8 +27,24 @@ public class Methods {
 
         System.out.println("What is the fourth grade ");
         grade3 = myScanner.nextInt();
+
+        totalGrade = grade0 + grade1 + grade2 + grade3;
+
+        newGPA = totalGrade/4;
+
+        System.out.println("Your GPA is " + newGPA);
     }
 }
+}
 
+public static void newLine(){
+    System.out.println();
+}
+
+public static void threeLine(){
+    newLine();
+    newLine();
+    newLine();
+}
 
 
