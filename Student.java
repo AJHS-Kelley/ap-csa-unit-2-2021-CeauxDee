b932dc6f2c36d94dc43a16d17f78a57ce85243a8
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-11-03 09:14:57 -0400

    [5B] v0.5
    Added a constructor

--- Student.java
+++ Student.java
@@ -6,3 +6,10 @@ public class Student{
     private double gradePointAvg;
     private String studentName;
 }
+
+public Student(){
+    this.studentNumber = 0;
+    this.gradeLevel = 0;
+    this.gradePointAvg = 0.0;
+    this.studentName = "";
+}
