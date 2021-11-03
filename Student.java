63f5a4525ba007f4d715e8eeabb407b77bdefd04
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-11-03 09:09:42 -0400

    [5B] v0.0
    Added private integers and a scanner

---
+++ Student.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Student{
+    private int studentNumber;
+    private int gradeLevel;
+    private double gradePointAvg;
+    private String studentName;
+}
