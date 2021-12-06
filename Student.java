653614355db7a4871bf52a69a63bfd2a0f223925
CeauxDee <81706887+CeauxDee@users.noreply.github.com>
2021-12-06 08:44:40 -0500

    [5B] v7.5
    Fixed GPA

--- Student.java
+++ Student.java
@@ -160,7 +160,7 @@ public static void mathPractice(){
         grade2 = 98;
 
         double fakeGPA;
-        fakeGPA = (grade0 + grade1 + grade2);
+        fakeGPA = (grade0 + grade1 + grade2) / 3.0;
 
         return fakeGPA;
     }
